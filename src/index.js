67bee4009d8cf5ff0303959f17b65ed00563a622
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-09-10 20:06:40 +0300

    Fetch data with useQuery

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import App from './App'
 
 const client = new ApolloClient({
   uri: 'https://flyby-router-demo.herokuapp.com/',
   cache: new InMemoryCache(),
 })
 
-client
-  .query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result))
+// Supported in React 18+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+)
